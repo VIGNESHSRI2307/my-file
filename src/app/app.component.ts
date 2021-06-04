71d753b64b7d6e6ac9e9c06bb94eff90a6673ed4
VIGNESHSRI2307 <85075260+VIGNESHSRI2307@users.noreply.github.com>
2021-06-04 12:25:27 +0530

    my-file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  profileForm=new FormGroup({
+    username: new FormControl(''),
+    email:new FormControl('')
+  })
+  
+  constructor(){
+    // console.log(this.username.value)
+  }
   title = 'my-file';
+  onSubmit(){
+    console.log("formsubmit")
+    console.log(this.profileForm.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +20,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
