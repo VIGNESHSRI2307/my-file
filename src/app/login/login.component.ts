55f13dd955b3dbbb52017b6f6743843ef5cb56d4
VIGNESHSRI2307 <85075260+VIGNESHSRI2307@users.noreply.github.com>
2021-06-05 09:09:14 +0530

    my-file

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,Validators,FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -7,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  // profileForm=new FormGroup({
+  //   username: new FormControl(''),
+  //   email:new FormControl('')
+  // })
+  submitted=false
+  constructor(private Fb:FormBuilder) {  
+}
+profileForm=this.Fb.group({
+  username:['',Validators.required],
+  email:['',[Validators.required,Validators.email]]
+})
+title = 'my-file';
+onSubmit(){
+  this.submitted=true
+  console.log(this.profileForm.valid)
+  if (this.profileForm.valid){console.log(this.profileForm.value)}
+  else {
+      alert("username,eamil,required")
+  }
+}
+get profileFormControl() {
+  return this.profileForm.controls;
+}
 
   ngOnInit(): void {
   }
+  
 
 }
